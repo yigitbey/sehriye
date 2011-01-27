af60e1d23e77ac6c83e3eafc962a72b380245336
Yiğit Güneli <gro.tiiy@yiit.org>
2011-01-27 20:18:49 +0000

    Adding initial version of pidgin + dbus based matching people

---
+++ sunucu/loop.py
@@ -0,0 +1,45 @@
+import dbus, dbus.glib, dbus.decorators, gobject , random
+
+bus = dbus.SessionBus() 
+obj = bus.get_object("im.pidgin.purple.PurpleService", "/im/pidgin/purple/PurpleObject") 
+purple = dbus.Interface(obj, "im.pidgin.purple.PurpleInterface") 
+
+
+protocols = {
+    "jabber": "prpl-jabber",
+} # List of protocols: http://developer.pidgin.im/wiki/prpl_id
+
+### Config starts ###
+account_name = "grotiiy@jabber.org" # Must already be logged on.
+account_protocol = "jabber" # Change this with the platform
+message_to_send = "test" # Change this with your spam
+### Config ends ###
+
+account = purple.PurpleAccountsFind(account_name, protocols[account_protocol]) # Get the account information
+buddylist = purple.PurpleFindBuddies(account,'') # Get a list of all buddies
+online_buddies = [] # Will contain online buddies
+
+# Search for online buddies, append them to online_buddies
+for buddy in buddylist:
+    if purple.PurpleBuddyIsOnline(buddy):
+        online_buddies.append(buddy)
+
+# Our function to handle a received message
+# TODO: Move to an abstract place.
+def randomPerson(account, sender, message, conversation, flags):
+    randP = random.choice(online_buddies) # Choose a buddy from online_buddies  
+    message_to = purple.PurpleBuddyGetName(randP) # Get buddy's instance
+    conversation = purple.PurpleConversationNew(1, account, message_to) # Create a new conversation with the receiver
+    purple.PurpleConvImSend(purple.PurpleConvIm(conversation), message) # Send the message to that conversation
+
+
+# Signal handler for ReceivedImMsg
+bus.add_signal_receiver(randomPerson,
+                        dbus_interface="im.pidgin.purple.PurpleInterface",
+                        signal_name="ReceivedImMsg") 
+
+# Main loop
+loop = gobject.MainLoop()
+loop.run()    
+
+
