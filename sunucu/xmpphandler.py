dbdc1d24b2162eb43c42310b304031ec88c82285
Yiğit Güneli <gro.tiiy@yiit.org>
2011-01-30 06:05:19 +0000

    Delete conversation session on DELETE_CONVERSATION

--- sunucu/xmpphandler.py
+++ sunucu/xmpphandler.py
@@ -2,6 +2,7 @@ from google.appengine.api import xmpp
 from google.appengine.ext import webapp
 
 from conversation import Conversation
+from custom_messages import *
 import logging, time
 
 class XMPPHandler(webapp.RequestHandler):
@@ -9,17 +10,23 @@ class XMPPHandler(webapp.RequestHandler):
     def post(self):
         """ Function to handle received messages """
         message = xmpp.Message(self.request.POST)
-        
-
-        sender = message.sender.split("/")[0]
+        sender = message.sender.split("/")[0] # Remove the identifier string from JID
         conversation = Conversation()
         partner = 0
-        while (partner == 0):
-            partner = conversation.matchPeople(sender)
+        while (partner == 0): # Until a partner is found, Google kills the loop if it takes too long
+            partner = conversation.matchPeople(sender) # Try to find a partner
+            if partner != 0: #If we have partner
+                if message.body == DELETE_CONVERSATION: #If a client wants to disconnect
+                    conversation.remove(sender,partner) # Remove the conversation session from database
+
+                # Send the message to partner even if the message is "/DELETETHISCONVERSATION"
+                # This is to make sure that both clients realize the disconnection
+                xmpp.send_message(partner, message.body) 
+
+                break # Break the partner searching loop, if any found
             time.sleep(1)
                     
 
-        xmpp.send_message(partner, message.body)
 
 
         
