7e2f6c9a3805fc9184c3f2ae81758f53d01b6607
Yiğit Güneli <gro.tiiy@yiit.org>
2011-02-01 03:02:27 +0000

    location setting with PENDING_CONVERSATION now works

--- sunucu/xmpphandler.py
+++ sunucu/xmpphandler.py
@@ -13,8 +13,6 @@ class XMPPHandler(webapp.RequestHandler):
       
         pass_message = 1 # Whether the message will be passed to partner
 
-        logging.debug("a"+message.body+str(pass_message))
-        logging.debug("b"+message.body.split(":")[0]+str(pass_message))
 
         #Disconnect
         if message.body == DELETE_CONVERSATION: #If a client wants to disconnect
@@ -22,12 +20,12 @@ class XMPPHandler(webapp.RequestHandler):
         
         #Pending Chat
         if message.body.split(":")[0] == PENDING_CONVERSATION: # If a client wants a conversation
-            partner = conversation.matchPeople(message.sender) # Try to find a partner
-            logging.debug("bey1:"+message.body)
+            sender = message.sender.split("/")[0] # Remove the identifier string from JID
+            partner = conversation.matchPeople(sender) # Try to find a partner
 
             if partner == 0 : #If we don't have partner, set the location of the conversation
                 conversation.setLocation(message.body.split(":")[1],message.body.split(":")[2])
-                logging.debug("bey:"+message.body)
+
 
             pass_message = 0 # Don't pass pending chat requests to clients
             
@@ -37,20 +35,23 @@ class XMPPHandler(webapp.RequestHandler):
             
     def post(self):
         """ Function to handle received messages """
+        dummy_email = "a@aa.aaa"
         message = xmpp.Message(self.request.POST)
-       
-        sender = message.sender.split("/")[0] # Remove the identifier string from JID
-        conversation = Conversation(user_1 = sender, user_2 = "a@aa.aaa",location = db.GeoPt(41,28))
+        pass_message = 1 
 
-        partner = conversation.matchPeople(sender) # Try to find a partner
+        sender = message.sender.split("/")[0] # Remove the identifier string from JID
+        conversation = Conversation(user_1 = sender, user_2 = dummy_email ,location = db.GeoPt(41,28))
         
-        pass_message = self.customMessageHandler(conversation,message)
+        partner = conversation.getPartner(sender) # Try to get a partner
+        if partner != 0:
+            conversation.user_2 = partner
+ 
+        if message.body[0] == "|": # If this is a custom message
+            pass_message = self.customMessageHandler(conversation,message)
 
         if partner != 0: #If we have partner
             
             # Initialize a conversation
-            conversation.user_1 = sender
-            conversation.user_2 = partner
    
             # Handling custom messages
            
