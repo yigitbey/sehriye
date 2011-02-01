903d579d5ae0519daa861f73c21cd62dd74ed36c
gungor <gngr.kck@gmail.com>
2011-02-01 02:02:07 +0200

    server model and serverGTalk created and populated

--- android/Tencere/src/com/android/tencere/model/server/Server.java
+++ android/Tencere/src/com/android/tencere/model/server/Server.java
@@ -1,5 +1,64 @@
 package com.android.tencere.model.server;
 
 public abstract class Server {
+	
+    protected String host;
+    protected String port; 
+    protected String service;
+    protected String username;
+    protected String password;
+
+
+	protected void setHost(String Host) {
+		this.host = Host;
+		
+	}
+
+	protected void setPort(String Port) {
+		this.port = Port;
+		
+	}
+
+
+	protected void setService(String Service) {
+		this.service = Service;
+		
+	}
+
+	protected void setUserName(String Username) {
+		this.username = Username;
+		
+	}
+
+	protected void setPassword(String Password) {
+		this.password = Password;
+		
+	}
+
+
+	public String getHost() {
+		return this.host;
+	}
+
+
+	public String getPort() {
+		return this.port;
+	}
+
+
+	public String getService() {
+		return this.service;
+	}
+
+
+	public String getUserName() {
+		return this.username;
+	}
+
+
+	public String getPassword() {
+		return this.password;
+	}
+
 
 }
--- android/Tencere/src/com/android/tencere/model/server/ServerGTalk.java
+++ android/Tencere/src/com/android/tencere/model/server/ServerGTalk.java
@@ -2,4 +2,15 @@ package com.android.tencere.model.server;
 
 public class ServerGTalk extends Server {
 
+	
+	public ServerGTalk(){
+		// TODO Auto-generated constructor stub
+    	setHost("talk.google.com");
+    	setPort("5222");
+    	setService("gmail.com");
+    	setUserName("at.avrat.kisnek");
+    	setPassword("m3rc1m3k");
+	}
+
+
 }
