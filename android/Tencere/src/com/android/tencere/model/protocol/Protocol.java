66529b8fda46479f26e3330ed7d978f3a80d7aba
gungor <gngr.kck@gmail.com>
2011-02-01 03:04:55 +0200

    model protocol and its message class defined

--- android/Tencere/src/com/android/tencere/model/protocol/Protocol.java
+++ android/Tencere/src/com/android/tencere/model/protocol/Protocol.java
@@ -1,5 +1,10 @@
 package com.android.tencere.model.protocol;
 
 public abstract class Protocol {
+	
+	protected static String chatIdentifier = "";
+	protected static String messageIdentifier = "";
+	protected static String parameterSplitter = "";
+	
 
 }
