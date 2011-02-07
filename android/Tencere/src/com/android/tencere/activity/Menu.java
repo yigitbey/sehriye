0e456fe2ebc92a72ca3626d3951a8d37dbbf2628
Yiğit Güneli <gro.tiiy@yiit.org>
2011-02-07 19:20:31 +0000

    new layout for application

--- android/Tencere/src/com/android/tencere/activity/Menu.java
+++ android/Tencere/src/com/android/tencere/activity/Menu.java
@@ -14,10 +14,6 @@ import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smack.util.StringUtils;
 
 import android.app.Activity;
-import android.content.Context;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -32,7 +28,7 @@ import com.android.tencere.activity.custom_messages;
 public class Menu extends Activity {
     private ArrayList<String> messages = new ArrayList();
     private Handler mHandler = new Handler();
-    private EditText mRecipient;
+
     private EditText mSendText;
     private ListView mList;
     private XMPPConnection connection;
