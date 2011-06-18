9d0eb350a28869c8a02eddf0d890f2588079f7b5
Yiğit Güneli <gro.tiiy@yiit.org>
2011-06-18 16:03:35 +0100

    new project for rewrite

---
+++ tencere2/src/com/mageroya/tencere2/Tencere2Activity.java
@@ -0,0 +1,13 @@
+package com.mageroya.tencere2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Tencere2Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
