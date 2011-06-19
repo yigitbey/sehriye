82b3ce67ade22afcb67c7f866b0298bd0546f1ee
Yiğit Güneli <gro.tiiy@yiit.org>
2011-06-19 23:58:31 +0100

    Adding constructor to User class

--- tencere2/src/com/mageroya/tencere2/User.java
+++ tencere2/src/com/mageroya/tencere2/User.java
@@ -2,13 +2,14 @@ package com.mageroya.tencere2;
 
 public abstract class User {
 	
-	public String name;
+    public String name;
     public String age;
     public String sex;
     public String location;
     public String phoneNumber;
     public String email;
-    
+    public String JID;
+
     abstract String getName();
     abstract String getAge();
     abstract String getSex();
@@ -16,5 +17,9 @@ public abstract class User {
     abstract String getPhoneNumber();
     abstract String getEmail();
     
+
+    public User(String JID){
+	this.JID = JID;
+    }
 }
     
