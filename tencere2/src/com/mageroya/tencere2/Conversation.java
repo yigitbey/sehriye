e99d9d3a9aecd43ee32c17915ffd9964e2a34c74
Yiğit Güneli <gro.tiiy@yiit.org>
2011-06-19 19:50:20 +0100

    implementing sendToPartner

--- tencere2/src/com/mageroya/tencere2/Conversation.java
+++ tencere2/src/com/mageroya/tencere2/Conversation.java
@@ -10,15 +10,27 @@ public class Conversation{
     public String dateJoined;
     public LocalUser localUser;
     public RemoteUser remoteUser;
-    
-    public Conversation(){
-	public ConnConfiguration connConfig = new ConnectionConfiguration(Jserver.host, Integer.parseInt(Jserver.port), Jserver.service);
-	public JServer jServer = Jserver(connConfig);
+    public ConnConfiguration connConfig;
+    public Jserver jserver;
+
+    public Conversation(LocalUser localUser, RemoteUser remoteUser){
+	this.connConfig = new ConnectionConfiguration(Jserver.host, Integer.parseInt(Jserver.port), Jserver.service);
+	this.jServer = Jserver(this.connConfig);
+	this.localUser = localUser;
+	this.remoteUser = remoteUser;
 	
     }
 
     public void SendMessage(String to, String text){
 	Message msg = new Message(to, Message.Type.chat);
 	msg.setBody(text);
-	jserver.sendPacket(msg);
+	this.jserver.sendPacket(msg);
     }
+
+    public void sendToPartner(String text){
+	Message msg = new Message(this.remoteUser, Message.Type.chat);
+	msg.setBody(text);
+	this.jserver.sendPacket(msg);
+    }
+
+}
