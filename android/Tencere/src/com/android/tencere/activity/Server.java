a89f078ea78754dbf86c7277af219f5ef3084007
Yiğit Güneli <gro.tiiy@yiit.org>
2011-07-09 16:14:34 +0100

    adding a new Server class

---
+++ android/Tencere/src/com/android/tencere/activity/Server.java
@@ -0,0 +1,9 @@
+package com.android.tencere.activity;
+
+public class Server {
+
+	final public static String agent = "buluruzbirsey@appspot.com";
+    final public static String host = "mageroya.com";
+    final public static String port = "5222";
+    final public static String service = "mageroya.com";
+}
