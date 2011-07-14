1ab7c043262b1a9b8d951c80a0cb6d2b33a20815
eren <eren@pavilyon.(none)>
2011-07-14 19:39:38 +0300

    switch new and end buttons' strings to strings.xml

--- android/Tencere/gen/com/android/tencere/activity/R.java
+++ android/Tencere/gen/com/android/tencere/activity/R.java
@@ -42,7 +42,10 @@ public final class R {
         public static final int multi_line_list_item=0x7f030001;
     }
     public static final class string {
+        public static final int addcontact=0x7f040002;
         public static final int app_name=0x7f040001;
+        public static final int endconv=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int newconv=0x7f040003;
     }
 }
